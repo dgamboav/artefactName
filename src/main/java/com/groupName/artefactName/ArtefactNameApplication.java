8cbb487352094da2b2c6c92c7abe6293d58fd434
dgamboav <diegoegamboa@gmail.com>
2025-04-05 15:23:34 -0400

    Base project

---
+++ src/main/java/com/groupName/artefactName/ArtefactNameApplication.java
@@ -0,0 +1,13 @@
+package com.groupName.artefactName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtefactNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtefactNameApplication.class, args);
+	}
+
+}
